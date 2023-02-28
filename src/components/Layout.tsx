4d7708c2513b102bae68f840aae0d5cd6aa0aa75
Alex Semenishchev <alexander.semenishchev@sbab.se>
2023-02-28 22:40:10 +0100

    styling & logo;

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,8 +1,11 @@
 import React from "react"
+import "./Layout.css"
 
 export default function Layout({ children }: any) {
   return (
-    <div style={{ margin: `0 auto`, maxWidth: 650, padding: `0 1rem` }}>
+    <div 
+      // style={{ margin: `0 auto`, maxWidth: 650, padding: `0 1rem` }} 
+      className="leaf-texture mx-auto my-0 max-w-lg px-5 py-4">
       {children}
     </div>
   )
--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -8,10 +8,10 @@ export default function TextInput({label, name, ...restProps}: any) {
   console.log(`${id}: `)
   return (
     <div className="mb-4">
-      <label className="block text-grey-darker text-sm font-bold mb-2 text-gray-500" htmlFor={id} >
+      <label className="block text-grey-darker text-sm uppercase font-bold mb-2 text-gray-500" htmlFor={id} >
         {label}
       </label>
-      <input className="shadow appearance-none border-2 border-gray-400 rounded w-full py-2 px-3 text-grey-darker" id={id} type="text" placeholder={placeholder} />
+      <input className="shadow appearance-none border-2 border-gray-300 rounded w-full py-2 px-3 text-grey-darker" id={id} type="text" placeholder={placeholder} />
     </div>
   )
 }
