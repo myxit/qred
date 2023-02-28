620a1d6e2af9d6a0a599082d6f65b162ce39b12e
Alex Semenishchev <alexander.semenishchev@sbab.se>
2023-02-28 08:32:30 +0100

    initial setup & form field setup

---
+++ src/components/TextInput.tsx
@@ -0,0 +1,17 @@
+import * as React from "react"
+import { v4 as uuidv4 } from 'uuid';
+
+export default function TextInput({label, name, ...restProps}: any) {  
+  const componentUUID = uuidv4()
+  const id = restProps.id || ['text', name.toLowerCase(), componentUUID].join('-')
+  const placeholder = restProps.placeholder || label.toLowerCase()
+  console.log(`${id}: `)
+  return (
+    <div className="mb-4">
+      <label className="block text-grey-darker text-sm font-bold mb-2 text-gray-500" htmlFor={id} >
+        {label}
+      </label>
+      <input className="shadow appearance-none border-2 border-gray-400 rounded w-full py-2 px-3 text-grey-darker" id={id} type="text" placeholder={placeholder} />
+    </div>
+  )
+}
